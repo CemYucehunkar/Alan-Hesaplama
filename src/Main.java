0ae31dcbfeb10941350d85b93b4b27df56d341b8
Cem Yücehünkar <pd.cemyucehunkar@gmail.com>
2023-09-15 06:38:21 +0300

    Alan Hesaplama

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        double c;
+
+        Scanner girdi=new Scanner(System.in);
+        System.out.print("1.Kenarı Giriniz:");
+        a=girdi.nextInt();
+
+        System.out.print("2.Kenarı Gir:");
+        b=girdi.nextInt();
+
+
+         c= Math.sqrt((a*a)+(b*b));
+        System.out.print("Hipotenüz Uzulunğu:"+c);
+
+
+
+    }
+}
